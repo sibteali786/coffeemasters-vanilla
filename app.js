777f81f49c57a2e8b820f23f8a319b7129bae0ac
Syed Sibteali Baqar <sibteali786@gmail.com>
2023-11-07 14:53:53 +0500

    Can add items to cart now

--- app.js
+++ app.js
@@ -34,3 +34,11 @@ window.addEventListener("DOMContentLoaded", () => {
   loadData();
   app.router.init();
 });
+
+window.addEventListener("appcartchange", (event) => {
+  console.log();
+  const badge = document.getElementById("badge");
+  const qty = app.store.cart.reduce((acc, item) => acc + item.quantity, 0);
+  badge.textContent = qty;
+  badge.hidden = qty === 0;
+});
--- components/ProductItem.js
+++ components/ProductItem.js
@@ -1,3 +1,5 @@
+import { addToCart } from "../services/Order.js";
+
 export default class ProductItem extends HTMLElement {
   constructor() {
     super();
@@ -14,9 +16,8 @@ export default class ProductItem extends HTMLElement {
     this.querySelector("p.price").textContent = `$${product.price.toFixed(2)}`;
     this.querySelector("img").src = `data/images/${product.image}`;
     this.querySelector("a").addEventListener("click", (event) => {
-      console.log(event.target.tagName);
       if (event.target.tagName.toLowerCase() == "button") {
-        //TODO
+        addToCart(product.id);
       } else {
         app.router.go(`/product-${product.id}`);
       }
